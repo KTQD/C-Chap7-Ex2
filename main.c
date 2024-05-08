281c291ea342a46cf5afd346764f6e6410c1ca54
Xuan Lam Pham <phamxuanlam@gmail.com>
2024-05-08 22:47:43 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,20 +2,7 @@
 #include <string.h>
 
 void reverseString(char* str) {
-    if (str == NULL)
-        return;
-
-    int length = strlen(str);
-    char* start = str;
-    char* end = str + length - 1;
-
-    while (start < end) {
-        char temp = *start;
-        *start = *end;
-        *end = temp;
-        start++;
-        end--;
-    }
+    
 }
 
 int main() {
